de0df3646fa3882b4f29417e7d959a2b7e96fbce
mercymekofficial <45404287+mercymekofficial@users.noreply.github.com>
2019-02-09 17:22:44 +0300

    Add files via upload

---
+++ Helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Every code works in a function. No code is able to work without a function. 
+// we may use "void" before any function to describe that it will not return any value.
+
+void main()
+{
+	printf("Hello World!"); // it is going to print this string on the screen.
+	getch(); // We use this to see the string before the screen turns off.
+}
