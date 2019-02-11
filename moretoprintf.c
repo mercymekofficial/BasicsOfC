df4e978c0adbb7aacef4b58e0099022f69b80841
mercymekofficial <45404287+mercymekofficial@users.noreply.github.com>
2019-02-11 12:32:07 +0300

    Add files via upload

---
+++ moretoprintf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int i,j,k;
+	printf("An \"escape sequence\" is a  series of characters that represents a special character.\n\t");
+	getch();
+	// We used \" to quote 2 of those words. 
+	// \n shows it is going to start a new line instead to continue in the same line. 
+	// \t will create a tab gap just like in real keyboard.
+}
